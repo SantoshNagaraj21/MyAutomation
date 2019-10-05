0f7c1e9259067fe1f4d87f5afdcdeb0fe2cf1ee1
SantoshNagaraj21 <sanbe.electronics@gmail.com>
2019-10-05 20:29:50 +0530

    updated Project

--- Automation/src/test/java/demo/api/test/dataProvDemo.java
+++ Automation/src/test/java/demo/api/test/dataProvDemo.java
@@ -9,63 +9,61 @@ import org.testng.annotations.Test;
 import aut.framework.common.BaseAPITest;
 import aut.framework.common.DataProviderClass;
 
-public class dataProvDemo extends BaseAPITest  {
-	
-	@Test(dataProvider="APITest",dataProviderClass=DataProviderClass.class)
-	public void ApiPostRequest(LinkedHashMap<String, String> dataMap) {
-		
-		Logger logger = Logger.getLogger("APITest");
-		
-		logger.info("---------------------------------------------------");
-		logger.info("Running test for Scenario APITest Through Excel DataProvider");
-		logger.info("---------------------------------------------------");
-		
-		// Get the data from excel
-		
-		String FirstName=dataMap.get("FirstName");
-		String LastName=dataMap.get("LastName");
-		String UserName=dataMap.get("UserName");
-		String Password=dataMap.get("Password");
-		String Email=dataMap.get("Email");
-		
-		System.out.println("Input Values " + FirstName + LastName + UserName + Password + Email);
-		
-	}
-	
-	@Test(enabled=true, dataProvider="APIXMLTest",dataProviderClass=DataProviderClass.class)
-	public void APiTest(LinkedHashMap<String, String> dataMap)  {
-		
-		Logger logger = Logger.getLogger("APITest");
+public class dataProvDemo extends BaseAPITest {
 
-		logger.info("---------------------------------------------------");
-		logger.info("Running test for Scenario api for XML");
-		logger.info("---------------------------------------------------");
+  @Test(dataProvider = "APITest", dataProviderClass = DataProviderClass.class)
+  public void ApiPostRequest(LinkedHashMap<String, String> dataMap) {
 
-		String FirstName=dataMap.get("firstname");
-		String ID=dataMap.get("id");
-		String lastname=dataMap.get("lastname");
-		String nickname=dataMap.get("nickname");
-		String marks=dataMap.get("marks");
-		
-		System.out.println("Input Values " + FirstName + ID + marks +  nickname +   lastname);
-		
-	}
-	
-	@Parameters({ "baseurl", "extendedUrl" })
-	@Test(enabled=true)
-	public void ApiPostRequest(String url, String exturl) {
+    Logger logger = Logger.getLogger("APITest");
 
-		Logger logger = Logger.getLogger("APITest");
+    logger.info("---------------------------------------------------");
+    logger.info("Running test for Scenario APITest Through Excel DataProvider");
+    logger.info("---------------------------------------------------");
 
-		logger.info("---------------------------------------------------");
-		logger.info("Running test for Scenario Data Provider through TestNg Parameters");
-		logger.info("---------------------------------------------------");
+    // Get the data from excel̥
 
-		System.out.println("Parameters from testNG Xml" + url + exturl);
+    String FirstName = dataMap.get("FirstName");
+    String LastName = dataMap.get("LastName");
+    String UserName = dataMap.get("UserName");
+    String Password = dataMap.get("Password");
+    String Email = dataMap.get("Email");
 
-	}
-	
-	
-	
+    System.out.println("Input Values " + FirstName + LastName + UserName + Password + Email);
+
+  }
+
+  @Test(enabled = true, dataProvider = "APIXMLTest", dataProviderClass = DataProviderClass.class)
+  public void APiTest(LinkedHashMap<String, String> dataMap) {
+
+    Logger logger = Logger.getLogger("APITest");
+
+    logger.info("---------------------------------------------------");
+    logger.info("Running test for Scenario api for XML");
+    logger.info("---------------------------------------------------");
+
+    String FirstName = dataMap.get("firstname");
+    String ID = dataMap.get("id");
+    String lastname = dataMap.get("lastname");
+    String nickname = dataMap.get("nickname");
+    String marks = dataMap.get("marks");
+
+    System.out.println("Input Values " + FirstName + ID + marks + nickname + lastname);
+
+  }
+
+  @Parameters({"baseurl", "extendedUrl"})
+  @Test(enabled = true)
+  public void ApiPostRequest(String url, String exturl) {
+
+    Logger logger = Logger.getLogger("APITest");
+
+    logger.info("---------------------------------------------------");
+    logger.info("Running test for Scenario Data Provider through TestNg Parameters");
+    logger.info("---------------------------------------------------");
+
+    System.out.println("Parameters from testNG Xml" + url + exturl);
+
+  }
+̥
 
 }
